37a69019536d183a88553b62c4574576e2ff0b2b
Brian Wandell <wandell@stanford.edu>
2016-11-20 09:07:04 -0800

    Updated

--- s_emcStimulus.m
+++ s_emcStimulus.m
@@ -9,91 +9,62 @@
 %% Init Parameters
 ieInit;
 
-%% Create scenes
+%% Here is a new function: oisCreate
 
-% To create each oiSequence we need two types of scenes: one with a
-% constant image and one with a time-varying presentation of the stimulus.
-scene = cell(2, 1);
+% We are going to mix a grating (harmonic) with a uniform background.
+% These are the relative weights of the grating and background.
+% Set up the timing
 
+% Gaussian onset and offset of the grating
+stimWeights = ieScale(fspecial('gaussian',[1,50],15),0,1);
+% Padded by zeroes
+weights = [zeros(1, 30), stimWeights, zeros(1, 30)];
+
+% This is the field of view of the scene.
+sparams.fov = 0.5;
+
+% Initialize the harmonic parameters structure with default
+% Change entries that are common to uniform and harmonic
 clear params
-params.freq =  10; % spatial frequencies of 1 and 5
-params.contrast = 0.9; % contrast of the two frequencies
-params.ang  = [0, 0]; % orientations
-params.ph  = [0 0]; % phase
-params.GaborFlag = 0.25;
-scene{1} = sceneCreate('harmonic',params);
-
-scene{1} = sceneSet(scene{1},'name',sprintf('F %d',params.freq));
-ieAddObject(scene{1});
-
-% Create scene: background field only, no harmonic
-clear params
-params.freq =  0; % spatial frequencies of 1 and 5
-params.contrast = 0; % contrast of the two frequencies
-params.ang  = [0, 0]; % orientations
-params.ph  = [0 0]; % phase
-scene{2} = sceneCreate('harmonic',params);
-scene{2} = sceneSet(scene{2},'name','Uniform');
-ieAddObject(scene{2});
-
-imgFov = .5 ;      % image field of view
-vDist  = 0.3;          % viewing distance (meter)
-
-% set scene fov
-for ii = 1 : length(scene)
-    scene{ii} = sceneSet(scene{ii}, 'h fov', imgFov);
-    scene{ii} = sceneSet(scene{ii}, 'distance', vDist);
+for ii=2:-1:1
+    params(ii) = harmonicP; 
+    params(ii).GaborFlag = 0.2;
+    params(ii).freq      = 10;
 end
-sceneWindow
 
-%% Compute human optical image sequences
+% params(1) is for the uniform field
+params(1).contrast  = 0.0;  % contrast of the two frequencies
 
-% Create a typical human lens.  It will be possible to set the parameters
-% for future experiments using
-%
-%   oi = oiCreate('wvf human',pupilMM,zCoefs,wave)
-% 
-% These are the default.
-oi = oiCreate('wvf human');
+% params(2) is matched and describes the grating
+params(2).contrast = 0.4;
 
-% Compute optical images from the scene
-OIs = cell(length(scene), 1);
-for ii = 1 : length(OIs)
-    OIs{ii} = oiCompute(oi,scene{ii});
-end
-% for ii=1:2, vcAddObject(OIs{ii}); end; oiWindow;
+% The call to create the retinal image sequence
+oisH = oisCreate('harmonic','blend',weights,'hparams',params,'sparams',sparams);
 
-%% Build the oiSequence
+% Have a look, though the code here is not that great yet so the look is
+% only approximate.
+oisH.visualize;
 
-% We build the stimulus using a time series of weights. We have the mean
-% field on for a while, then rise/fall, then mean field.
-zTime = 50;   % Mean field beginning and end (ms)
-stimWeights = fspecial('gaussian',[1,50],15);
-stimWeights = ieScale(stimWeights,0,1);
-weights = [zeros(1, zTime), stimWeights, zeros(1, zTime)];
+%% Now, make the cone mosaic and compute absorptions and current
 
-% Temporal samples.  Typically 1 ms, which is set by the parameter in the
-% cone mosasic integration time.  That time is locked to the eye movements.
-tSamples = length(weights); 
-sampleTimes = 0.002*(1:tSamples);  % Time in sec
-
-% vcNewGraphWin; plot(1:tSamples, weights,'o');
-% xlabel('Time (ms)');
-
-% The weights define some amount of the constant background and some amount
-% of the line on the same constant background
-oiHarmonicSeq = oiSequence(OIs{2}, OIs{1}, ...
-    sampleTimes, weights, ...
-    'composition', 'blend');
-oiHarmonicSeq.visualize('format','movie');
-
-%% Set up the photon absorptions from the sequence
+fov = oiGet(oisH.oiFixed,'fov');
+tSamples = oisH.length;
 
 cMosaic = coneMosaic;
-cMosaic.integrationTime = 0.002;
-cMosaic.setSizeToFOV(0.5);
+cMosaic.integrationTime = 0.001;
+cMosaic.setSizeToFOV(fov);
+
+% Possibly, we should control the eye movement sequence
 cMosaic.emGenSequence(tSamples);
-cMosaic.compute(oiHarmonicSeq);
+
+% Compute and then look
+cMosaic.compute(oisH);
+cMosaic.window;
+
+% Compute and look
+cMosaic.os.noiseFlag = true;
+cMosaic.computeCurrent;
+cMosaic.window;
 
 %% Create the current with and without noise
 
@@ -101,11 +72,7 @@ cMosaic.os.noiseFlag = false;
 cMosaic.computeCurrent;
 cMosaic.window;
 
-%% These are the impulse response functions
-
-cMosaic.plot('os current filters');
-
-%% Adjust the eye movement parameters
+%% How to adjust the eye movement parameters
 
 % Pretty big tremor
 em = emCreate;     % Create an eye movement object
@@ -123,3 +90,5 @@ em.tremor.amplitude = 0.005;
 cMosaic.emGenSequence(tSamples,'em',em);
 cMosaic.plot('eye movement path');
 set(gca,'xlim',[-15 15],'ylim',[-15 15]);
+
+%%
