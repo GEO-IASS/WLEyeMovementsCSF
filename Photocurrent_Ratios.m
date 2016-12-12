32b903fbb95117b897382a708796ac7015859943
Brian Wandell <wandell@stanford.edu>
2016-12-11 19:03:30 -0800

    Added comments.  Fixed demeaning for modern methods with bsxfun

--- Photocurrent_Ratios.m
+++ Photocurrent_Ratios.m
@@ -1,5 +1,9 @@
+%% Comments here
 
+%%
 ieInit;
+
+%% Initialize parameters
 
 % Gaussian onset and offset of the grating
 stimWeights = ieScale(fspecial('gaussian',[1,50],15),0,1);
@@ -14,70 +18,80 @@ sparams.fov = 0.5;
 freq = [2 30 40];
 ratios = zeros(3,1);
 for a = 1:length(freq)
-clear params
-for ii=2:-1:1
-    params(ii) = harmonicP; 
-    params(ii).GaborFlag = 0.2;
-    params(ii).freq      = freq(a);
-    params(ii).row = 256;
-    params(ii).col = 256;
+    clear params
+    for ii=2:-1:1
+        params(ii) = harmonicP;
+        params(ii).GaborFlag = 0.2;
+        params(ii).freq      = freq(a);
+        params(ii).row = 256;
+        params(ii).col = 256;
+    end
+    
+    % params(1) is for the uniform field
+    params(1).contrast  = 0.0;  % contrast of the two frequencies
+    
+    % params(2) is matched and describes the grating
+    params(2).contrast = 1;
+    
+    % The call to create the retinal image sequence
+    oisH = oisCreate('harmonic','blend',weights,'testParameters',params,'sceneParameters',sparams);
+    
+    fov = oiGet(oisH.oiFixed,'fov');
+    tSamples = oisH.length;
+    
+    cMosaic = coneMosaic;
+    cMosaic.integrationTime = 0.001;
+    cMosaic.setSizeToFOV(fov);
+    
+    % create em object without movement
+    em_noMovement = emCreate;     % Create an eye movement object
+    em_noMovement.emFlag = [0 0 0];  % Make sure tremor, draft and saccade are all off
+    cMosaic.emGenSequence(tSamples,'em',em_noMovement);  % Generate the sequence
+    cMosaic.compute(oisH);
+    cMosaic.computeCurrent;
+    
+    deMeanedMosaic = bsxfun(@minus,cMosaic.current,mean(cMosaic.current,3));
+    % deMeanedMosaic = cMosaic.current-mean(cMosaic.current,3);
+
+    padNumFrames = 2^nextpow2(length(cMosaic.current(1,1,:)));
+    Spectra_noMovements = abs(fftshift(fft(deMeanedMosaic,padNumFrames,3)));
+    avgSpectrum_noMovements = squeeze(sum(sum(Spectra_noMovements)))./93^2;
+    
+    % create em object with movement
+    emF = 3; emA = 3;
+    x = round(emA*sin(2*pi*emF*(1:tSamples)/tSamples));
+    y = zeros(size(x(:)));
+    cMosaic.emPositions = [x(:),y(:)];
+    cMosaic.name = 'Horizontal em';
+    cMosaic.plot('eye movement path');
+    cMosaic.compute(oisH);
+    cMosaic.computeCurrent;
+    
+    deMeanedMosaic = bsxfun(@minus,cMosaic.current,mean(cMosaic.current,3));
+    % deMeanedMosaic = cMosaic.current-mean(cMosaic.current,3);
+    Spectra_withMovements = abs(fftshift(fft(deMeanedMosaic,padNumFrames,3)));
+    avgSpectrum_withMovements = squeeze(sum(sum(Spectra_withMovements)))./93^2;
+    
+    avgSpectraAmp = sum(sum(Spectra_withMovements./Spectra_noMovements))./93^2;
+    figure
+    plot(squeeze(avgSpectraAmp(64:128))) % plot positive frequency ratios
+    str = sprintf('spatial freq = %d',freq(a));
+    title(str);
+    ratios(a) = avgSpectraAmp(66);
 end
 
-% params(1) is for the uniform field
-params(1).contrast  = 0.0;  % contrast of the two frequencies
-
-% params(2) is matched and describes the grating
-params(2).contrast = 1;
-
-% The call to create the retinal image sequence
-oisH = oisCreate('harmonic','blend',weights,'hparams',params,'sparams',sparams);
-
-fov = oiGet(oisH.oiFixed,'fov');
-tSamples = oisH.length;
-
-cMosaic = coneMosaic;
-cMosaic.integrationTime = 0.001;
-cMosaic.setSizeToFOV(fov);
-
-% create em object without movement
-em_noMovement = emCreate;     % Create an eye movement object
-em_noMovement.emFlag = [0 0 0];  % Make sure tremor, draft and saccade are all off
-cMosaic.emGenSequence(tSamples,'em',em_noMovement);  % Generate the sequence
-cMosaic.compute(oisH);
-cMosaic.computeCurrent;
-deMeanedMosaic = cMosaic.current-mean(cMosaic.current,3);
-padNumFrames = 2^nextpow2(length(cMosaic.current(1,1,:)));
-Spectra_noMovements = abs(fftshift(fft(deMeanedMosaic,padNumFrames,3)));
-avgSpectrum_noMovements = squeeze(sum(sum(Spectra_noMovements)))./93^2;
-
-% create em object with movement
-emF = 3; emA = 3;
-x = round(emA*sin(2*pi*emF*(1:tSamples)/tSamples));
-y = zeros(size(x(:)));
-cMosaic.emPositions = [x(:),y(:)]; 
-cMosaic.name = 'Horizontal em';
-cMosaic.plot('eye movement path');
-cMosaic.compute(oisH);
-cMosaic.computeCurrent;
-deMeanedMosaic = cMosaic.current-mean(cMosaic.current,3);
-Spectra_withMovements = abs(fftshift(fft(deMeanedMosaic,padNumFrames,3)));
-avgSpectrum_withMovements = squeeze(sum(sum(Spectra_withMovements)))./93^2;
-
-avgSpectraAmp = sum(sum(Spectra_withMovements./Spectra_noMovements))./93^2;
-figure
-plot(squeeze(avgSpectraAmp(64:128))) % plot positive frequency ratios
-str = sprintf('spatial freq = %d',freq(a));
-title(str);
-ratios(a) = avgSpectraAmp(66);
-end
-figure
+%% Plot results
+vcNewGraphWin;
 plot(squeeze(sum(sum((Spectra_noMovements)))))
-figure
+
+
+vcNewGraphWin;
 plot(squeeze(sum(sum((Spectra_withMovements)))))
 ratios
+
 for i = 1:10
-figure
-plot(squeeze(Spectra_noMovements(5*i,5*i,:)))
-figure
-plot(squeeze(Spectra_withMovements(5*i,5*i,:)))
+    vcNewGraphWin;
+    plot(squeeze(Spectra_noMovements(5*i,5*i,:)))
+    vcNewGraphWin;
+    plot(squeeze(Spectra_withMovements(5*i,5*i,:)))
 end
